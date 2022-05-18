d99884ba1909d85f21acef0d54a0e09de354965e
Daniel Burger <48986191+danielburger1337@users.noreply.github.com>
2022-05-18 14:53:00 +0200

    added main bundle entrypoint

---
+++ src/SBSEDVInputConverterBundle.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+
+namespace SBSEDV\Bundle\InputConverterBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class SBSEDVInputConverterBundle extends Bundle
+{
+}
