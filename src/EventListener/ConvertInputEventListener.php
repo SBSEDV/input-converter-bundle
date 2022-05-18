7f9c276ab62b2b6c30eb6c6d6ea30fd0936bbe6f
Daniel Burger <48986191+danielburger1337@users.noreply.github.com>
2022-05-18 14:53:34 +0200

    added main event subscriber

---
+++ src/EventListener/ConvertInputEventListener.php
@@ -0,0 +1,51 @@
+<?php declare(strict_types=1);
+
+namespace SBSEDV\Bundle\InputConverterBundle\EventListener;
+
+use SBSEDV\Component\InputConverter\Exception\MalformedContentException;
+use SBSEDV\Component\InputConverter\Exception\UnsupportedRequestException;
+use SBSEDV\Component\InputConverter\InputConverter;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\Exception\BadRequestException;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+
+final class ConvertInputEventListener implements EventSubscriberInterface
+{
+    public function __construct(
+        private InputConverter $inputConverter
+    ) {
+    }
+
+    /**
+     * Handle the "kernel.request" event.
+     *
+     * @param RequestEvent $event The "kernel.request" event.
+     */
+    public function onKernelRequest(RequestEvent $event): void
+    {
+        if (!$event->isMainRequest()) {
+            return;
+        }
+
+        $request = $event->getRequest();
+
+        try {
+            $parsedInput = $this->inputConverter->convert($request);
+            $parsedInput->applyOnHttpFoundationRequest($request);
+        } catch (MalformedContentException $e) {
+            throw new BadRequestException($e->getMessage(), previous: $e);
+        } catch (UnsupportedRequestException) {
+            // do nothing
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            RequestEvent::class => ['onKernelRequest', 4096],
+        ];
+    }
+}
